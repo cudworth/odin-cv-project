087d9e21f4635cf430e30de9b920a9cf4a0c6049
cudworth <austincudworth@gmail.com>
2022-01-03 21:41:28 -0800

    Initialize form and rendering class-based React components.

---
+++ src/components/CVForm.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+import "./CVForm.css";
+
+class CVForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div class="cv-form">Hello from the CVForm component</div>;
+  }
+}
+
+export default CVForm;
---
+++ src/components/RenderCV.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+import "./RenderCV.css";
+
+class RenderCV extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div class="render-cv">Hello from RenderCV</div>;
+  }
+}
+
+export default RenderCV;
